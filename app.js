8e5c03e107f149deeab3075183643dc2ee8043b3
Lukas Kliševičius <lukas.klisevicius@inspired.local>
2022-05-24 10:57:17 +0300

    banneris

---
+++ app.js
@@ -0,0 +1,8 @@
+const close_btn = document.querySelector("#close")
+const banner = document.querySelector('#banner')
+
+function close_banner(){
+    close_btn.style.display = 'none'
+    banner.style.display = 'none'
+
+}
